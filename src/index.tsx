71f88ac2d34906ca39ab37b296da8c92ed2d818c
argon1738 <Eivind.antoniazzi@argonville.no>
2025-07-17 18:29:11 +0200

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx'; // Endring her
+import MainApp from './MainApp.tsx'; // Endret
 import './index.css';
 
 const rootElement = document.getElementById('root');
@@ -11,6 +11,6 @@ if (!rootElement) {
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <MainApp />
   </React.StrictMode>
 );
